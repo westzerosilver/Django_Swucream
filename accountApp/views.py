8a7dc2287c2e0ce5976863cc0f2ce7ea6e9a1a37
westzerosilver <oooiooop9988@gmail.com>
2024-03-31 19:19:26 +0900

    django courser 9: include && extends

--- accountApp/views.py
+++ accountApp/views.py
@@ -7,4 +7,5 @@ from django.shortcuts import render
 # hello world를 출력하는 함수
 def hello_world(request) :
     #return HttpResponse('Hello World!')    # 응답을 view에서 직접 만들어주는 방식
-    return render(request, 'base.html')     # 템플릿을 가지고 와서 내용을 채워주는 형식
+    #return render(request, 'base.html')     # 템플릿을 가지고 와서 내용을 채워주는 형식
+    return render(request, 'accountApp/hello_world.html')   # accountApp 내의 html 파일 불러옴
