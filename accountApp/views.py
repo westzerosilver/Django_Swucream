0dc340af0665678398f19868e2fcfe72bf358e38
westzerosilver <oooiooop9988@gmail.com>
2024-03-31 18:53:53 +0900

    djust Django course 8 commit

--- accountApp/views.py
+++ accountApp/views.py
@@ -6,4 +6,5 @@ from django.shortcuts import render
 # Create your views here.
 # hello world를 출력하는 함수
 def hello_world(request) :
-    return HttpResponse('Hello World!')
+    #return HttpResponse('Hello World!')    # 응답을 view에서 직접 만들어주는 방식
+    return render(request, 'base.html')     # 템플릿을 가지고 와서 내용을 채워주는 형식
